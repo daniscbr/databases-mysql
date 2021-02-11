4520c2ac6302ff369a4d274082566acaef05205a
Daniela <dani.scbr0@gmail.com>
2021-02-11 11:15:56 -0600

    Schema complete

--- server/schema.sql
+++ server/schema.sql
@@ -2,10 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE users (
   /* Describe your table here.*/
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+
 );
 
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  users_id INT NOT NULL,
+  rooms_id INT NOT NULL,
+  text VARCHAR(255) NOT NULL,
+  CONSTRAINT
+    FOREIGN KEY (users_id)
+    REFERENCES users(id),
+  CONSTRAINT
+    FOREIGN KEY (rooms_id)
+    REFERENCES rooms(id)
+);
 /* Create other tables and define schemas for them here! */
 
 
